3ae3bff7c334b02eb0c16c78ff33d54be6135eea
marcgumo <marc.guerrero.moreno@gmail.com>
2022-04-27 15:25:48 +0200

    Insertion sort implementado

--- ProyectoSorting/Program.cs
+++ ProyectoSorting/Program.cs
@@ -139,9 +139,20 @@ namespace ProyectoSorting
 
         public void InsertionSort(int[] array)
         {
-
+            for (int i = 1; i < array.Length; i++)
+            {
+                int temp = array[i];
+                int j = i - 1;
+                while (j > 0 && array[j] > temp)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = temp;
+            }
         }
     }
+
     class Program
     {
         static void Main(string[] args)
